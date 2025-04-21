2854f2cbf8b0efc9a644d86e88a335ea20d136c7
Sam Chris <samlearningcode14@gmail.com>
2025-04-21 08:03:12 +0300

    included console.error to check my error is db

--- server/database/db.js
+++ server/database/db.js
@@ -5,7 +5,7 @@ const connectToDb = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully");
   } catch (error) {
-    console.log("Connection failed");
+    console.log(`Connection failed ${error.message}`);
     process.exit(1);
   }
 };
